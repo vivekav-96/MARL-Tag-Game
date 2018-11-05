0791fcfaa8d22178c261a887ca97bf59f065c616
Vivek <vivekav.96@gmail.com>
2018-11-05 13:27:57 +0530

    Added doc comments.

--- agent.py
+++ agent.py
@@ -13,36 +13,52 @@ class Agent(ABC):
         self.parent = parent
 
     def gogogo(self):
-        t = threading.Thread(target=self.__start)
+        """
+        Starts Random Movement.
+        """
+        t = threading.Thread(target=self.__start_random)
         t.start()
 
-    """
-    Starts Random Movement.
-    """
-
-    def __start(self):
+    def __start_random(self):
         x = random.randint(-10, 10)
         y = random.randint(-10, 10)
         i = 0
         while True:
-            coords = self.parent.coords(self.card)
             if i % 10 == 0:
                 x = random.randint(-10, 10)
                 y = random.randint(-10, 10)
-            x = x if 15 < coords[0] + x < self.parent.winfo_width() - 15 else 0
-            y = y if 15 < coords[1] + y < self.parent.winfo_height() - 15 else 0
             self.move(x, y)
-            time.sleep(0.1)
+            time.sleep(0.05)
             i += 1
 
     def move(self, x, y):
-        self.parent.move(self.card, x, y)
-        pass
+        """
+        :param x: Distance to move in X direction
+        :param y: Distance to move in Y direction
+        :return: None
+
+        The actual_x and actual_y are calculated so as that the agent won't move outside the screen
+        actual_x and actual_y are the distances actually in X and Y directions respectively.
+        """
+        coords = self.get_self_coords()
+        actual_x = x if 15 < coords[0] + x < self.parent.winfo_width() - 15 else 0
+        actual_y = y if 15 < coords[1] + y < self.parent.winfo_height() - 15 else 0
+        self.parent.move(self.card, actual_x, actual_y)
 
     def get_self_coords(self):
+        """
+        :return: X, Y coordinates of the agent
+        """
         return self.parent.coords(self.card)
 
     def get_angle_to_target(self, target_x, target_y):
+        """
+        :param target_x: Target X co-ordinate
+        :param target_y: Target Y co-ordinate
+        :return: Euclidean angle to th target.
+
+        Divide by Zero error cases are avoided.
+        """
         coords = self.get_self_coords()
         if target_x == coords[0] and target_y > coords[1]:
             print('case 1 : {}'.format(-90))
@@ -56,4 +72,8 @@ class Agent(ABC):
 
     @abstractmethod
     def get_icon(self):
+        pass
+
+    @abstractmethod
+    def step(self, env):
         pass
