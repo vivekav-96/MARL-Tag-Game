d793b4ec6499f734270f3faab9445548031b2d86
Vivek <vivekav.96@gmail.com>
2018-11-08 08:31:04 +0530

    test.py is the new main

--- tag-environment/tag_environment/envs/tag_env.py
+++ tag-environment/tag_environment/envs/tag_env.py
@@ -15,8 +15,8 @@ class TagEnv(gym.Env):
 
         self.update_ui()
 
-        self.box = (self.canvas.winfo_x(), self.canvas.winfo_y(),
-                    self.canvas.winfo_rootx(), self.canvas.winfo_rooty())
+        self.box = (self.canvas.winfo_rootx(), self.canvas.winfo_rooty(),
+                    self.canvas.winfo_width(), self.canvas.winfo_height())
 
     def step(self, action):
         pass
@@ -40,3 +40,13 @@ class TagEnv(gym.Env):
 
     def get_canvas(self):
         return self.canvas
+
+    # def callback(self):
+    #     print('  root.geometry:', self.root.winfo_geometry())
+    #     print('canvas.geometry:', self.canvas.winfo_geometry())
+    #     print('canvas.width :', self.canvas.winfo_width())
+    #     print('canvas.height:', self.canvas.winfo_height())
+    #     print('canvas.x:', self.canvas.winfo_x())
+    #     print('canvas.y:', self.canvas.winfo_y())
+    #     print('canvas.rootx:', self.canvas.winfo_rootx())
+    #     print('canvas.rooty:', self.canvas.winfo_rooty())
